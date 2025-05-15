3d2470a900bda56820cf2c764f69bf133a27d306
infracdo <infracdo@gmail.com>
2025-05-15 08:30:09 +0000

    add dockerfile

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://"+window.location.hostname+":7547",
+  baseURL: "http://192.168.43.108:7547",
   headers: {
     "Content-type": "application/json",
   }
