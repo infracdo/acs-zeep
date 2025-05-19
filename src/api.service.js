70f76927880b0d39277c49f9bd1ae28311135896
infra <infracdo@gmail.com>
2025-05-19 09:48:52 +0800

    add radius

--- src/api.service.js
+++ src/api.service.js
@@ -2,40 +2,78 @@ import http from './http-common';
 
 /* eslint-disable class-methods-use-this */
 class ApiService {
-  getCountConnectedUsers() {
-    return http.get('/api/count-current-connected-users');
+  // FOR CAPTIVE PORTAL (WIFIDOG) DATA RETRIEVAL APIs
+  // getCountCurrentlyConnectedUsers() {
+  //   return http.get('/api/count-current-connected-users');
+  // }
+
+  // getCountCurrentlyConnectedAPs() {
+  //   return http.get('/api/count-current-connected-aps');
+  // }
+
+  // getTotalUserConnectionsToday() {
+  //   return http.get('/api/total-user-connections-today');
+  // }
+
+  // getTotalBandwidthConsumptionToday() {
+  //   return http.get('/api/total-bandwidth-consumption-today');
+  // }
+
+  // getAverageConnectionTime() {
+  //   return http.get('/api/avg-connection-time');
+  // }
+
+  // getAverageBandwidthPerConnection() {
+  //   return http.get('/api/average-bandwidth-per-connection');
+  // }
+
+  // getCountCurrentlyConnectedUsersPerAP() {
+  //   return http.get('/api/count-current-connected-users-per-ap');
+  // }
+
+  // getCurrentlyConnectedUsersPerAP() {
+  //   return http.get('/api/current-connected-users-per-ap');
+  // }
+
+  // getAccessPoints() {
+  //   return http.get('/api/current-connected-aps');
+  // }
+
+  // FOR RADIUS DATA RETRIEVAL APIs
+  getCountCurrentlyConnectedUsers() {
+    return http.get('/api/radius/count-currently-connected-users');
   }
 
-  getCountCurrentConnectedAPs() {
-    return http.get('/api/count-current-connected-aps');
+  getCountCurrentlyConnectedAPs() {
+    return http.get('/api/radius/count-currently-connected-aps');
   }
 
   getTotalUserConnectionsToday() {
-    return http.get('/api/total-user-connections-today');
+    return http.get('/api/radius/total-user-connections-today');
   }
 
   getTotalBandwidthConsumptionToday() {
-    return http.get('/api/total-bandwidth-consumption-today');
+    return http.get('/api/radius/total-bandwidth-consumption-today');
   }
 
-  getAvgConnectionTime() {
-    return http.get('/api/avg-connection-time');
+  getAverageConnectionTime() {
+    return http.get('/api/radius/average-connection-time');
   }
 
   getAverageBandwidthPerConnection() {
-    return http.get('/api/average-bandwidth-per-connection');
+    return http.get('/api/radius/average-bandwidth-per-connection');
   }
 
   getCountCurrentlyConnectedUsersPerAP() {
-    return http.get('/api/count-current-connected-users-per-ap');
+    return http.get('/api/radius/count-currently-connected-users-per-ap');
   }
 
-  getCurrentConnectedUsersPerAP() {
-    return http.get('/api/current-connected-users-per-ap');
+  getCurrentlyConnectedUsersPerAP() {
+    return http.get('/api/radius/currently-connected-users-per-ap');
   }
 
-  getCurrentConnectedAPs() {
-    return http.get('/api/current-connected-aps');
+  getAccessPoints() {
+    return http.get('/api/radius/access-points');
   }
 }
 /* eslint-enable class-methods-use-this */
